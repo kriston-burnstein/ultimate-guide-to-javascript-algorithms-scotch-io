49cfb99c734d6149436dfb685c4c36bc5122ebe9
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-19 14:16:45 -0400

    Add imperative solution to wordConceal challenge

--- Challenges/wordConceal/index-START.js
+++ Challenges/wordConceal/index-START.js
@@ -11,9 +11,34 @@ For words that begin with vowel sounds, simply add "way" to the end of the word.
 "explain" = "explainway”
 */
 
-function wordConceal (str) {
-    //code goes here
-}
-
+//Imperative solution (shows the steps of how to solve)
+function wordConceal(str) {
+    // Convert string to lowercase
+    str = str.toLowerCase()
+    // Initialize array of vowels
+    const vowels = ["a", "e", "i", "o", "u"];
+    // Initialize vowel index to 0
+    let vowelIndex = 0;
+  
+    if (vowels.includes(str[0])) {
+      // If first letter is a vowel
+      return str + "way";
+    } else {
+      // If the first letter isn't a vowel i.e is a consonant
+      for (let char of str) {
+        // Loop through until the first vowel is found
+        if (vowels.includes(char)) {
+          // Store the index at which the first vowel exists
+          vowelIndex = str.indexOf(char);
+          break;
+        }
+      }
+      // Compose final string
+      return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+    }
+  }
+  console.log(wordConceal("big")) // = "igbay"
+  console.log(wordConceal("glove")) // = "oveglay"
+  console.log(wordConceal("explain")) // = "explainway”
 
 module.exports = wordConceal
