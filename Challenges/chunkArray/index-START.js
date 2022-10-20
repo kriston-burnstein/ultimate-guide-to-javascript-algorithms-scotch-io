5c0cf98a6f3406a4134b4d4f27244101a0a2e73c
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-20 18:36:15 -0400

    Add two additional solutions with comments to chunkArray challenge

--- Challenges/chunkArray/index-START.js
+++ Challenges/chunkArray/index-START.js
@@ -24,6 +24,9 @@ function chunkArray(bigArray, chunkSize) {
 //Looping through the number of chunks
 //.splice()
 //.splice takes the starting index, and the number of elements to be extracted and returned
+//.splice is used to modify the content of an array by removing or replacing elements
+//here, we'll remove elements
+//.splice returns the removed elements
 //.splice manipulates an array in place, so we'll make a copy so our operations don't affect the data received
 function chunkArray(bigArray, chunkSize) {
     let result = []
@@ -32,6 +35,30 @@ function chunkArray(bigArray, chunkSize) {
         result.push(arrCopy.splice(0, chunkSize))
     }
     return result
+}
+
+//For loop with .slice()
+//.slice returns a shallow copy of a portion of an array
+//takes a starting index, an ending index (not inclusive)
+//we'll increment our iterator by the specified chunkSize on each iteration in order to start each new group from where the previous one ended 
+
+function chunkArray(bigArray, chunkSize) {
+    let result = [] 
+    for (let i = 0; i < bigArray.length; i += chunkSize) {
+        let chunk = bigArray.slice(i, i + chunkSize)
+        result.push(chunk)
+    }
+    return result
+}
+
+//Recursion with .slice()
+function chunkArray(bigArray, chunkSize) {
+    //terminating condition so we get balance of big array back at end when it's less than or equal to our desired chunkSize
+    if (bigArray.length <= chunkSize) {
+        return [bigArray] 
+    }
+    //we use .slice() to extract portions of the array as chunks as well as reduce the size of the original array continuously until the terminating condition is met
+    return [bigArray.slice(0, chunkSize), ...chunkArray(bigArray.slice(chunkSize), chunkSize)]
 }
 
 console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5))
