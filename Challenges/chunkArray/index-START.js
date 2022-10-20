0422db497a651684cac5f273b391e73b956d0aa5
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-20 14:50:55 -0400

    Edit/Add chunkArray challenge description

--- Challenges/chunkArray/index-START.js
+++ Challenges/chunkArray/index-START.js
@@ -1,11 +1,12 @@
 /* 
- Given two or more arrays, write a function that combines
- their elements into one array without any repetition. 
- E.g  mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
+ Given an array and a chunk size, divide the array into many sub-arrays where each sub-array has the specified length. 
+ 
+ E.g. chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5)
+ should return [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]
 */
 
 function chunkArray(array, size) {
-    // Code goes here
+    //code goes here
 }
 
 
