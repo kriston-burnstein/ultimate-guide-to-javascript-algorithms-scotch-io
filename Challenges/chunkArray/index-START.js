734ebca80f649071229d6b61e0bcba5cfb28bcc9
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-20 18:05:45 -0400

    Add two solutions to chunkArray challenge

--- Challenges/chunkArray/index-START.js
+++ Challenges/chunkArray/index-START.js
@@ -5,9 +5,36 @@
  should return [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]
 */
 
-function chunkArray(array, size) {
-    //code goes here
+//return an array of sub-arrays, on sub-array for every 5 elements
+
+//For of loop
+function chunkArray(bigArray, chunkSize) {
+    let result = []
+    for (el of bigArray) {
+        let lastArray = result[result.length - 1]
+        if (!lastArray || lastArray.length == chunkSize) {
+            result.push([el])
+        }else {
+            lastArray.push(el)
+        }
+    }
+    return result
 }
 
+//Looping through the number of chunks
+//.splice()
+//.splice takes the starting index, and the number of elements to be extracted and returned
+//.splice manipulates an array in place, so we'll make a copy so our operations don't affect the data received
+function chunkArray(bigArray, chunkSize) {
+    let result = []
+    let arrCopy = [...bigArray]
+    while (arrCopy.length > 0) {
+        result.push(arrCopy.splice(0, chunkSize))
+    }
+    return result
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5))
+// [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]
 
 module.exports = chunkArray
