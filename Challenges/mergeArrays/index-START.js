df09d0fd39e57083c93883cecd1d08b338196636
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-24 16:16:57 -0400

    Add speed test results in comments of mergeArrays

--- Challenges/mergeArrays/index-START.js
+++ Challenges/mergeArrays/index-START.js
@@ -111,3 +111,7 @@ function mergeArrays(...arrays) {  //rest, of all the arrays
 }
 
 module.exports = mergeArrays
+
+//Speed Test Results
+//According to scotch.io tests indicate the .filter() method is the fastest approach of all three. Next is using a set.
+
