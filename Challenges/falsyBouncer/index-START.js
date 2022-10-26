340a755cb1398e7686a3ba0147ce0c391ac6f789
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-26 16:43:08 -0400

    Edit challenge description in falsyBouncer

--- Challenges/falsyBouncer/index-START.js
+++ Challenges/falsyBouncer/index-START.js
@@ -3,6 +3,8 @@ Given an array, remove all falsy values from the array
 and return an array of only truthy values.
 
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
+
+Of note: Examples of falsy values are: false, null, undefined, 0, NaN, and "".
 */
 
 
