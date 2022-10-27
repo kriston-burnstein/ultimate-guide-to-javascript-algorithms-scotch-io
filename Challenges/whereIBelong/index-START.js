1a43f46e68cdc40838737481d7e474e5d8453b60
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-27 19:27:41 -0400

    Add for loop challenge solution to whereIBelong

--- Challenges/whereIBelong/index-START.js
+++ Challenges/whereIBelong/index-START.js
@@ -14,6 +14,23 @@ function whereIBelong(arr, num) {
    return arr.sort((a, b) => a - b).indexOf(num)
 }
 
+//Using for loop
+//The logic here is that when the array is sorted in ascending order, we can determine what position the specified value should be injected into, by determining the value that’d be immediately after it in terms of size
+
+// We start by using the .sort() method to arrange the array values in ascending order. Next, use a for-loop to iterate through the sorted array while comparing each value with the specified number num. If the value is greater than num, we return the value of the iterator i, which is also the index at which the first larger value was found.
+
+// In a situation where none of the values are larger, it means that the specified value will come at the end. So we return the length of the array which is also the index of the next position to be filled.
+
+function whereIBelong(arr, num) {
+   arr.sort((a,b) => a - b)
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > num) {
+         return i
+      }
+   }
+   return arr.length
+}
+
 console.log(whereIBelong([1,2,3,4], 1.5)) // should return 1 (index)
 
 module.exports = whereIBelong
