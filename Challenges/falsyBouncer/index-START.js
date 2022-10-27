9115cfa175a0bf05f3eaac144c6efdc72958ee63
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-27 17:00:59 -0400

    Add filter method challenge solution for falsyBouncer

--- Challenges/falsyBouncer/index-START.js
+++ Challenges/falsyBouncer/index-START.js
@@ -7,17 +7,21 @@ E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 Of note: Examples of falsy values are: false, null, undefined, 0, NaN, and "".
 */
 
-//Using for of loop
-function falsyBouncer(array) {
-    let filteredArray = []
-    for (let value of array) {
-        if (value) {
-            filteredArray.push(value)
-        }
-    }
-    return filteredArray
-}
+// //Using for of loop
+// function falsyBouncer(array) {
+//     let filteredArray = []
+//     for (let value of array) {
+//         if (value) {
+//             filteredArray.push(value)
+//         }
+//     }
+//     return filteredArray
+// }
 
+//Using .filter()
+function falsyBouncer(array) {
+    return array.filter(el => Boolean(el))
+}
 
 console.log(falsyBouncer([1, 0, null, '', 5])) // should return [1,5]
 
