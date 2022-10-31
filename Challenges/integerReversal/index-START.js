6b21e845613df2268a70e6f46c3d877e1e0f85bf
Kriston Burnstein <kriston.burnstein@gmail.com>
2022-10-31 16:31:44 -0400

    Add integerReversal challenge files and challenge description

---
+++ Challenges/integerReversal/index-START.js
@@ -0,0 +1,17 @@
+/*
+Given an integer, return an integer that has the reverse ordering of that which was received. E.g
+
+reverseInteger(-123) // should return -321
+Keep the following rules in mind:
+
+The signs must not change i.e -123 becomes -321
+The value returned must be an integer.
+All insignificant zeroes must be removed i.e 900 becomes 9 and not 009
+*/
+ 
+function reverseString(text) {
+  
+}
+
+
+//module.exports = integerReversal
